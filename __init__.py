ab5f7db6d64ebbed7a2b960d93294a19b3738934
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2024-12-30 22:10:31 +0200

    init

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .HunyuanVideoStyler import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+# This enables auto-discovery of the nodes
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
